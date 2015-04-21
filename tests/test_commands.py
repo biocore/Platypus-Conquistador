0fa41ce98028ba4a527f8302b1d2c5f5dbe079d4
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-04-20 22:34:38 -0600

    tst: Add tests for compare

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -15,7 +15,7 @@ from click import BadParameter
 from platypus.commands import split_db
 
 
-class TestCommands(TestCase):
+class TestSplitDB(TestCase):
     def setUp(self):
         self.base = abspath(join(dirname(__file__), 'support_files'))
         self.tax_fp = join(self.base, 'small-bacteria.contigs.txt')
@@ -72,6 +72,40 @@ class TestCommands(TestCase):
 
         with open(exp_rest) as exp, open(out_rest) as out:
             self.assertItemsEqual(exp.readlines(), out.readlines())
+
+class TestCompare(TestCase):
+    def setUp(self):
+        self.base = abspath(join(dirname(__file__), 'support_files'))
+        self.interest_fp = join(self.base, 'first_db.txt')
+        self.other_fp = join(self.base, 'second_db.txt')
+
+        self.to_delete = []
+
+    def tearDown(self):
+        for d in self.to_delete:
+            try:
+                rmtree(d)
+            except OSError:
+                pass
+
+    def test_compare_base(self):
+        temp_dir = gettempdir()
+        self.to_delete.append(temp_dir)
+
+        compare(self.interest_fp, self.other_fp, temp_dir)
+
+        files = ['compile_output.txt', 'compile_output_no_nohits.txt',
+                 'summary_p1_70-a1_50_p2_70-a2_50.txt']
+        for fp in files:
+            exp_fp = join(self.base, 'compare-tests', fp)
+            out_fp = join(temp_dir, fp)
+
+            with open(exp_fp) as exp, open(out_fp) as out:
+                self.assertItemsEqual(exp.readlines(), out.readlines())
+
+    def test_compare_exceptions(self):
+        pass
+
 
 if __name__ == '__main__':
     main()
