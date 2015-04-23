3d1f0be4c3805419b69cf997109108c5b945764e
Daniel McDonald <mcdonadt@colorado.edu>
2015-04-23 08:50:32 -0600

    REL: license updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------------
 # Copyright (c) 2015--, platypus development team.
 #
-# Distributed under the terms of the GPL License.
+# Distributed under the terms of the BSD License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
@@ -12,13 +12,14 @@ from distutils.core import setup
 
 classes = """
     Development Status :: 2 - Pre-Alpha
-    License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+    License :: OSI Approved :: BSD License
     Topic :: Software Development :: Libraries :: Application Frameworks
     Programming Language :: Python
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: Implementation :: CPython
     Operating System :: OS Independent
     Operating System :: POSIX
+    Operating System :: POSIX :: Linux
     Operating System :: MacOS :: MacOS X
 """
 classifiers = [s.strip() for s in classes.split('\n') if s]
@@ -38,7 +39,7 @@ setup(name='platytpus-conquistador',
       maintainer='Antonio Gonzalez Pena',
       maintainer_email='antgonza@gmail.com',
       url='http://github.com/biocore/platypus',
-      license='GPL',
+      license='BSD',
       packages=['platypus'],
       scripts=['scripts/platypus'],
       install_requires=base,
